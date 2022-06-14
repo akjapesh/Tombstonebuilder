b6ef043ab26db4a69d6450b263b91e42d5bb4f63
Abhinav Japesh <abhinav.japesh@sprinklr.com>
2022-06-14 23:09:33 +0530

    useEffect, setcordes, canvasaddprops in canvas.js

--- tombbuilder/src/components/Canvas.js
+++ tombbuilder/src/components/Canvas.js
@@ -5,11 +5,45 @@ function Canvas() {
   const [tool, setTool] = useState(Tools.Select);
   const [coordsActiveItem, setCordState] = useState({});
   const sketchProperty = useRef(null);
- 
+  const canvasAddedProp = (target) => {
+    const newTarget = target;
+    const hasCircle =
+      newTarget &&
+      (newTarget.type === "circle" ||
+        (newTarget.type === "activeSelection" &&
+          newTarget._objects.some((o) => o.type === "circle")));
+    if (hasCircle) {
+      newTarget.lockUniScaling = true;
+      newTarget.lockRotation = true;
+      newTarget.angle = 0;
+      newTarget.originY = "top";
+    }
 
-   
- 
-  
+    return newTarget;
+  };
+  const setCoords = ({ target }) => {
+    const { type, width, height, left, top, radius, rx } = target;
+
+    if (type === "circle") {
+      return setCordState({ coordsActiveItem: { radius, left, top, type } });
+    }
+
+    return setCordState({
+      coordsActiveItem: { width, height, left, top, boxRadius: rx, type }
+    });
+  };
+  useEffect(() => {
+    sketchProperty.current._fc.on({
+      "selection:created": (item) => {
+        setCoords(item);
+        item.target = canvasAddedProp(item.target);
+      },
+      "selection:updated": setCoords,
+      "selection:cleared": () => setCordState({ coordsActiveItem: {} }),
+      "object:moving": (item) => (item.target = canvasAddedProp(item.target)),
+      "object:added": (item) => (item.target = canvasAddedProp(item.target))
+    });
+  }, []);
   useEffect(() => {
     console.log(sketchProperty.current._fc._objects);
   });
