6f17f499649257c73d701b88dccce3cf42de11d8
Abhinav Japesh <abhinav.japesh@sprinklr.com>
2022-06-20 15:29:52 +0530

    grid step move on drag

--- tombbuilder/src/components/canvas/hooks/useCanvasActions/useActiveItem.js
+++ tombbuilder/src/components/canvas/hooks/useCanvasActions/useActiveItem.js
@@ -1,19 +1,22 @@
 import { useCallback, useState } from "react";
 
 const DEFAULT_COORDS = {};
-
+const GRID_STEP=4;
 export const useActiveItem = () => {
   const [activeItemCoords, setActiveItemCoords] = useState(DEFAULT_COORDS);
   const setCoords = useCallback(
     (target) => {
       const { type, width, height, left, top, radius, rx } = target;
+      const newleft=Math.floor(left/GRID_STEP)*GRID_STEP;
+      const newtop=Math.floor(top/GRID_STEP)*GRID_STEP;
+      console.log(newleft,newtop)
       if (type === "circle") {
         return setActiveItemCoords({
-          activeItemCoords: { radius, left, top, type },
+          activeItemCoords: { radius, left:newleft, top:newtop, type },
         });
       }
       return setActiveItemCoords({
-        activeItemCoords: { width, height, left, top, boxRadius: rx, type },
+        activeItemCoords: { width, height, left:newleft, top:newtop, boxRadius: rx, type },
       });
     },
     [setActiveItemCoords]
