dc2e3c8ec90f39d31e3daadb4b295cbfa054ba74
Abhinav Japesh <abhinav.japesh@sprinklr.com>
2022-06-21 14:40:16 +0530

    bug of resizing shapes content loader resizing fixed

--- tombbuilder/src/utils/annotationsToCode.js
+++ tombbuilder/src/utils/annotationsToCode.js
@@ -26,9 +26,9 @@ export const annotationsToCode = (annotationArray, contentLoaderState) => {
   }
 
   annotationArray.forEach((a) => {
-    const height = numberFixed(a.height);
+    const height = numberFixed(a.height*a.scaleY);
 
-    const width = numberFixed(a.width);
+    const width = numberFixed(a.width*a.scaleX);
 
     if ((height === 0 && width === 0) || a.r === 1) {
       return null;
@@ -43,9 +43,9 @@ export const annotationsToCode = (annotationArray, contentLoaderState) => {
       \n`;
     } else if (a.type === "circle") {
       code += `   
-       <circle cx="${numberFixed(a.left) + numberFixed(a.radius)}" cy="${
-        numberFixed(a.top) + numberFixed(a.radius)
-      }" r="${numberFixed(a.radius)}" /> 
+       <circle cx="${numberFixed(a.left) + numberFixed(a.radius*a.scaleY)}" cy="${
+        numberFixed(a.top) + numberFixed(a.radius*a.scaleY)
+      }" r="${numberFixed(a.radius*a.scaleX)}" /> 
       \n
       `;
     }
