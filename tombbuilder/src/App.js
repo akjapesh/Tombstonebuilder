85516d95419a631e363b78acd33d7a41d7df3dec
Soham Patel <soham.patel@sprinklr.com>
2022-06-21 11:31:17 +0530

    merged soham  with abhinav for grid

--- tombbuilder/src/App.js
+++ tombbuilder/src/App.js
@@ -12,7 +12,7 @@ import { useAnnotaionToCanvas } from "./components/canvas/hooks/useAnnotationToC
 //Components
 import Canvas from "./components/canvas/Canvas";
 import CanvasConfiguration from "./components/canvasConfiguration/CanvasConfiguration";
-import Editor from "./components/Editor/Editor";
+import Editor from "./components/editor/Editor";
 
 //styles
 import "./styles/styles.css";
