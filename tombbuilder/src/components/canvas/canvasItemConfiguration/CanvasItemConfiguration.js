5048128aa4de2475639696f9c650d2c034e79962
Sanyam Srivastava <105932014+Sanyamsri@users.noreply.github.com>
2022-06-22 11:16:58 +0530

    removed the React button from the code

--- tombbuilder/src/components/canvas/canvasItemConfiguration/CanvasItemConfiguration.js
+++ tombbuilder/src/components/canvas/canvasItemConfiguration/CanvasItemConfiguration.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-direct-mutation-state */
 import React from "react";
 import { Button } from "baseui/button";
 // import { Input } from "baseui/input";
@@ -14,75 +13,42 @@ function CanvasItemConfiguration({
 }) {
   const disableKeyEvents = () => {
     document.removeEventListener("keydown", handleKeyDown, false);
-    // console.log(activeItemCoords);
+    console.log(activeItemCoords);
   };
 
   return (
     <div className="app-editor_item-editor">
       <p className="app-config_caption">Size & position of active item</p>
-<<<<<<< HEAD
-      <div className="row">
-        {/* eslint-disable react/no-direct-mutation-state */}
-        {/* <button disabled={!activeItemCoords.activeItemCoords} onClick={handleRemoveItemFromKeyboard}>DELETE</button> */}
-
-        <span>
-          <Button onClick={handleRemoveItemFromKeyboard}>Delete</Button>
-          {/* eslint-disable react/no-direct-mutation-state */}
-          <Button onClick={handleCloneItem}>copy</Button>
-          {/* eslint-disable react/no-direct-mutation-state */}
-=======
       <div className="row ">
         {/* <button disabled={!activeItemCoords.activeItemCoords} onClick={handleRemoveItemFromKeyboard}>DELETE</button> */}
 
         <span>
-          <Button className="app-handler__trash" onClick={handleRemoveItemFromKeyboard}><img src={trashIcon} alt="remove item" /></Button>
-          <Button className="app-handler__clone" onClick={handleCloneItem}><img src={cloneIcon} alt="clone tool" /></Button>
->>>>>>> origin/abhinav
+          <Button
+            className="app-handler__trash"
+            onClick={handleRemoveItemFromKeyboard}
+          >
+            <img src={trashIcon} alt="remove item" />
+          </Button>
+          <Button className="app-handler__clone" onClick={handleCloneItem}>
+            <img src={cloneIcon} alt="clone tool" />
+          </Button>
         </span>
+
         {Object.keys(activeItemCoords)
-          .filter((e) => e!== 'type' && e !== undefined)
+          .filter((e) => e !== "type" && e !== undefined)
           .map((item) => {
-            let value = numberFixed(activeItemCoords[item]);
-            
-            const onChange = (e) => {                                       //  changed as we have to convert cx,cy into left right
-              const maxLimit = 1000;                                        //  hard coded maxLIMIT
-              let NEW_VALUE = e.target.value;
-              if(item ==='cx')
-              {
-                item = 'left';
-                NEW_VALUE = Number(e.target.value - activeItemCoords['radius']);
-              }
-              else if(item ==='cy')
-              {
-                item='top';
-                NEW_VALUE = Number(e.target.value - activeItemCoords['radius']);
-              }
-
-              NEW_VALUE = Math.max(NEW_VALUE-e.target.value,Math.min(maxLimit,NEW_VALUE));
-              handleMoveItem(item,NEW_VALUE);
+            const value = numberFixed(activeItemCoords[item]);
+            const onChange = (e) => {
+              handleMoveItem(item, numberFixed(Number(e.target.value)));
             };
-
-            if(activeItemCoords['type']==='circle' && item!=='radius')     //  changed as we have to convert cx,cy into left right
-            {
-              if(item==='left'){
-              item = 'cx';
-              }
-              else if(item==='top'){
-              item = 'cy';
-              }
-              value= value + Number(activeItemCoords['radius']);
-            }
-
             return (
               <p className="app-config_inline" key={item}>
                 <label>{item}</label>
-                {/* eslint-disable react/no-direct-mutation-state */}
                 <input
                   type="number"
                   onChange={onChange}
                   value={value}
-                  // onKeyDown={disableKeyEvents}
-                  onFocus={disableKeyEvents}
+                  onKeyDown={disableKeyEvents}
                 />
               </p>
             );
