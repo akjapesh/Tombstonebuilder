9ab3b1be5bc3bf5bb1a833dda7e83293c2f11cc1
Sanyam Srivastava <105932014+Sanyamsri@users.noreply.github.com>
2022-06-23 14:29:13 +0530

    fixed(multiple selection): used toJSON method to store objects
    fixed the problem of annotations shifting by using toJSON method to store objects

--- tombbuilder/src/components/canvas/hooks/useCanvasActions/useSetupCanvas.js
+++ tombbuilder/src/components/canvas/hooks/useCanvasActions/useSetupCanvas.js
@@ -15,7 +15,7 @@ export const useSetupCanvas = (
         handleToolChange(tools.Select);
       },
       "after:render": () => {
-        updateAnnotationHandler([...sketchRef.current._fc._objects]);
+        updateAnnotationHandler([...sketchRef.current._fc.toJSON().objects]);
       },
       "selection:created": (item) => {
         setCoords(item.selected[0]);
@@ -29,7 +29,6 @@ export const useSetupCanvas = (
       },
       "object:modified": (item) => {
         setCoords(item.target);
-        updateAnnotationHandler([...sketchRef.current._fc._objects]);
       },
       "object:added": (item) =>
         (item.target = handleAddItemInCanvas(item.target)),
