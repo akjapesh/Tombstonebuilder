0def29372353860d928176be5776b19c933ab986
Soham Patel <soham.patel@sprinklr.com>
2022-06-23 10:54:22 +0530

    added gearbox-icon for modal and use StatefulTooltip.

--- tombbuilder/src/components/canvas/modalExample/ModalExample.js
+++ tombbuilder/src/components/canvas/modalExample/ModalExample.js
@@ -7,6 +7,8 @@ import {
   ModalFooter,
   ModalButton,
 } from "baseui/modal";
+import gearIcon from "../../../assets/gear.svg";
+import { StatefulTooltip } from "baseui/tooltip";
 
 export default function ModalExample({ children, header }) {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -18,7 +20,13 @@ export default function ModalExample({ children, header }) {
   return (
     <React.Fragment>
       <Button onClick={() => setIsOpen(true)}>
-        Size & position of active item
+            <StatefulTooltip
+              content={() => (  <p>"Click to configure"</p>)}
+              returnFocus
+              autoFocus>
+                <img src={gearIcon} alt="config of active item"></img>
+            </StatefulTooltip>
+
       </Button>
       <Modal onClose={close} isOpen={isOpen}>
         <ModalHeader>CONFIGURATIONS</ModalHeader>
