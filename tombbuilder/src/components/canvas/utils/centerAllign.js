9dd65a302c8277be400dc60020c76cd626c6452a
Sanyam Srivastava <105932014+Sanyamsri@users.noreply.github.com>
2022-06-25 11:08:32 +0530

    fixe(utils and canvas): Removed Bugs of file name and refactor
    removed some bugs and some changes due to styling configuration being different in prettier

--- tombbuilder/src/components/canvas/utils/centerAllign.js
+++ tombbuilder/src/components/canvas/utils/centerAllign.js
@@ -1,61 +1,79 @@
-
 import { fabric } from "fabric";
 import { numberFixed } from "../../../utils/handleFixingNumbers";
 import { SHIFTING_BY_OFFSET } from "../hooks/useSetKeyEvents/useSetKeyPressActions/useArrowKeysNavigation";
 
+export const centerAllign = (sketchRef) => {
+  const calculateCenter = (target) => {
+    const center = { centerX: 0, centerY: 0 };
+    // center.centerY=numberFixed(numberFixed(target.top) - (numberFixed(target.top) % SHIFTING_BY_OFFSET)+(numberFixed(target.height) - (numberFixed(target.height) % SHIFTING_BY_OFFSET))/2);
+    // center.centerX=numberFixed(numberFixed(target.left) - (numberFixed(target.left) % SHIFTING_BY_OFFSET)+(numberFixed(target.width) - (numberFixed(target.width) % SHIFTING_BY_OFFSET))/2);
+    const newTop =
+      numberFixed(target.top) - (numberFixed(target.top) % SHIFTING_BY_OFFSET);
+    const newLeft =
+      numberFixed(target.left) -
+      (numberFixed(target.left) % SHIFTING_BY_OFFSET);
+    const newHeight =
+      numberFixed(target.height) -
+      (numberFixed(target.height) % SHIFTING_BY_OFFSET);
+    const newWidth =
+      numberFixed(target.width) -
+      (numberFixed(target.width) % SHIFTING_BY_OFFSET);
+    center.centerY = newTop + newHeight / 2;
+    center.centerX = newLeft + newWidth / 2;
+    center.centerX = numberFixed(center.centerX);
 
-export const centerAllign=(sketchRef)=>{
+    center.centerY = numberFixed(center.centerY);
 
-const calculateCenter=(target)=>{
-    const center={centerX:0,centerY:0};
-      // center.centerY=numberFixed(numberFixed(target.top) - (numberFixed(target.top) % SHIFTING_BY_OFFSET)+(numberFixed(target.height) - (numberFixed(target.height) % SHIFTING_BY_OFFSET))/2);
-      // center.centerX=numberFixed(numberFixed(target.left) - (numberFixed(target.left) % SHIFTING_BY_OFFSET)+(numberFixed(target.width) - (numberFixed(target.width) % SHIFTING_BY_OFFSET))/2);
-      const newTop=numberFixed(target.top)-numberFixed(target.top)%SHIFTING_BY_OFFSET;
-      const newLeft=numberFixed(target.left)-numberFixed(target.left)%SHIFTING_BY_OFFSET;
-      const newHeight=numberFixed(target.height)-numberFixed(target.height)%SHIFTING_BY_OFFSET;
-      const newWidth=numberFixed(target.width)-numberFixed(target.width)%SHIFTING_BY_OFFSET;
-      center.centerY=newTop+newHeight/2;
-      center.centerX=newLeft+newWidth/2;
-      center.centerX=numberFixed(center.centerX)-numberFixed(center.centerX)%SHIFTING_BY_OFFSET;
-
-      center.centerY=numberFixed(center.centerY)-numberFixed(center.centerY)%SHIFTING_BY_OFFSET;
-    
     return center;
-  }
+  };
 
-
-  const clearCenterAllignLines=()=>{
-    sketchRef.current._fc._objects.forEach((o)=>{
-      if(o.type==="line"){
+  const clearCenterAllignLines = () => {
+    sketchRef.current._fc._objects.forEach((o) => {
+      if (o.type === "line") {
         sketchRef.current._fc.remove(o);
       }
     });
-  
-  
-  }
+  };
 
-
-  const connectCenterAllignLine=(target)=>{
+  const connectCenterAllignLine = (target) => {
     clearCenterAllignLines();
-    const targetCenter=calculateCenter(target);
-    sketchRef.current._fc._objects.forEach(o => {
-      if(o!==target){
-      const anotherShapeCenter=calculateCenter(o);
-      if(targetCenter.centerX===anotherShapeCenter.centerX){
-        sketchRef.current._fc.add(new fabric.Line([targetCenter.centerX,targetCenter.centerY,anotherShapeCenter.centerX,anotherShapeCenter.centerY],{stroke:"red",}));
-        // console.log("----------Y line---------",center,prCenter);
+    const targetCenter = calculateCenter(target);
+    sketchRef.current._fc._objects.forEach((o) => {
+      if (o !== target) {
+        const anotherShapeCenter = calculateCenter(o);
+        if (targetCenter.centerX === anotherShapeCenter.centerX) {
+          sketchRef.current._fc.add(
+            new fabric.Line(
+              [
+                targetCenter.centerX,
+                targetCenter.centerY,
+                anotherShapeCenter.centerX,
+                anotherShapeCenter.centerY,
+              ],
+              { stroke: "red" }
+            )
+          );
+          // console.log("----------Y line---------",center,prCenter);
+        } else if (targetCenter.centerY === anotherShapeCenter.centerY) {
+          sketchRef.current._fc.add(
+            new fabric.Line(
+              [
+                targetCenter.centerX,
+                targetCenter.centerY,
+                anotherShapeCenter.centerX,
+                anotherShapeCenter.centerY,
+              ],
+              { stroke: "red" }
+            )
+          );
+          // console.log("---------X line----------",center,prCenter);
+        }
       }
-      else if(targetCenter.centerY===anotherShapeCenter.centerY){
-        sketchRef.current._fc.add(new fabric.Line([targetCenter.centerX,targetCenter.centerY,anotherShapeCenter.centerX,anotherShapeCenter.centerY],{stroke:"red"}));
-        // console.log("---------X line----------",center,prCenter);
-      }
-    }
     });
-  
-  }
+  };
 
   return {
     clearCenterAllignLines,
     connectCenterAllignLine,
   };
-}
+};
