6cf1b1dcdb03d18f61ee56c27dfb78eafa1eebdc
MihirGH <hashmihir@gmail.com>
2022-06-25 16:09:26 +0530

    chore: rename directories to avoid casing issues

---
+++ tombbuilder/src/components/editor/reactCodeEditor/utils/formatCode.js
@@ -0,0 +1,10 @@
+import prettier from "prettier/esm/standalone.mjs";
+import PRETTIER_CODE_FORMATTER_PROPERTIES from "./PrettierCodeFormatterProperties";
+
+export const formatCode = (code) => {
+  const newCode = prettier.format(code, {
+    ...PRETTIER_CODE_FORMATTER_PROPERTIES,
+  });
+
+  return newCode;
+};
