513db65019abbf8b307aeee888de72371cee29fc
Sanyam Srivastava <105932014+Sanyamsri@users.noreply.github.com>
2022-06-28 17:36:28 +0530

    fixed(handlsharecode):removed hostname

--- tombbuilder/src/utils/handleShareCode.js
+++ tombbuilder/src/utils/handleShareCode.js
@@ -1,7 +1,5 @@
 export const handleShareCode = (event, annotation) => {
   event.preventDefault();
-  const nextUrl = `${window.location.hostname}/?data=${btoa(
-    JSON.stringify(annotation)
-  )}`;
+  const nextUrl = `/?data=${btoa(JSON.stringify(annotation))}`;
   window.history.replaceState({}, "", nextUrl);
 };
