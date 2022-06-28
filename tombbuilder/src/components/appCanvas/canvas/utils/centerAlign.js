d6d1ade68f5cd560dd1eb3c9b07a9d85f1167969
Abhinav Japesh <abhinav.japesh@sprinklr.com>
2022-06-28 13:54:37 +0530

    connecting edges of shapes

--- tombbuilder/src/components/appCanvas/canvas/utils/centerAlign.js
+++ tombbuilder/src/components/appCanvas/canvas/utils/centerAlign.js
@@ -1,26 +1,24 @@
 import { fabric } from "fabric";
 import { numberFixed } from "utils/handleFixingNumbers";
-import { SHIFTING_BY_OFFSET } from "../hooks/useSetKeyEvents/useSetKeyPressActions/useArrowKeysNavigation";
+import { shiftValueByOffset } from "utils/shiftValueByOffset";
 
 export const centerAlign = (sketchRef) => {
   const calculateCenter = (target) => {
-    const center = { centerX: 0, centerY: 0 };
-    const newTop =
-      numberFixed(target.top) - (numberFixed(target.top) % SHIFTING_BY_OFFSET);
-    const newLeft =
-      numberFixed(target.left) -
-      (numberFixed(target.left) % SHIFTING_BY_OFFSET);
-    const newHeight =
-      numberFixed(target.height) -
-      (numberFixed(target.height) % SHIFTING_BY_OFFSET);
-    const newWidth =
-      numberFixed(target.width) -
-      (numberFixed(target.width) % SHIFTING_BY_OFFSET);
+    const center = { centerX: 0, centerY: 0 ,left:0,right:0,top:0,down:0};
+    const newTop =shiftValueByOffset(numberFixed( target.top));
+    const newLeft =shiftValueByOffset(numberFixed(target.left));
+    const newHeight =shiftValueByOffset(numberFixed(target.height));
+    const newWidth =shiftValueByOffset(numberFixed(target.width));
+
     center.centerY = newTop + newHeight / 2;
     center.centerX = newLeft + newWidth / 2;
     center.centerX = numberFixed(center.centerX);
     center.centerY = numberFixed(center.centerY);
-
+    center.left=shiftValueByOffset(numberFixed(target.left));
+    center.right=shiftValueByOffset(numberFixed(target.left))+shiftValueByOffset(numberFixed(target.width));
+    center.top=shiftValueByOffset(numberFixed(target.top));
+    center.down=shiftValueByOffset(numberFixed(target.top))+shiftValueByOffset(numberFixed(target.height))
+   console.log(center);
     return center;
   };
 
@@ -36,9 +34,10 @@ export const centerAlign = (sketchRef) => {
     clearCenterAlignLines();
     const targetCenter = calculateCenter(target);
     sketchRef.current._fc._objects.forEach((o) => {
-      if (o !== target) {
+      if (o !== target && o.type!=="line") {
         const anotherShapeCenter = calculateCenter(o);
-        if (targetCenter.centerX === anotherShapeCenter.centerX) {
+        if (targetCenter.centerX === anotherShapeCenter.centerX)
+          {
           sketchRef.current._fc.add(
             new fabric.Line(
               [
@@ -50,8 +49,8 @@ export const centerAlign = (sketchRef) => {
               { stroke: "red" }
             )
           );
-          // console.log("----------Y line---------",center,prCenter);
-        } else if (targetCenter.centerY === anotherShapeCenter.centerY) {
+        } 
+        else if(targetCenter.centerY===anotherShapeCenter.centerY){
           sketchRef.current._fc.add(
             new fabric.Line(
               [
@@ -63,7 +62,111 @@ export const centerAlign = (sketchRef) => {
               { stroke: "red" }
             )
           );
-          // console.log("---------X line----------",center,prCenter);
+        }
+        
+        else if(targetCenter.top===anotherShapeCenter.top){
+          sketchRef.current._fc.add(
+            new fabric.Line(
+              [
+                targetCenter.centerX,
+                targetCenter.top,
+                anotherShapeCenter.centerX,
+                anotherShapeCenter.top,
+              ],
+              { stroke: "red" }
+            )
+          );
+        } 
+        else if(targetCenter.left===anotherShapeCenter.left){
+          sketchRef.current._fc.add(
+            new fabric.Line(
+              [
+                targetCenter.left,
+                targetCenter.centerY,
+                anotherShapeCenter.left,
+                anotherShapeCenter.centerY,
+              ],
+              { stroke: "red" }
+            )
+          );
+        } 
+        else if(targetCenter.down===anotherShapeCenter.down){
+          sketchRef.current._fc.add(
+            new fabric.Line(
+              [
+                targetCenter.centerX,
+                targetCenter.down,
+                anotherShapeCenter.centerX,
+                anotherShapeCenter.down,
+              ],
+              { stroke: "red" }
+            )
+          );
+        } 
+        else if(targetCenter.right===anotherShapeCenter.right){
+          sketchRef.current._fc.add(
+            new fabric.Line(
+              [
+                targetCenter.right,
+                targetCenter.centerY,
+                anotherShapeCenter.right,
+                anotherShapeCenter.centerY,
+              ],
+              { stroke: "red" }
+            )
+          );
+        }
+        else if(targetCenter.top===anotherShapeCenter.down){
+          sketchRef.current._fc.add(
+            new fabric.Line(
+              [
+                targetCenter.centerX,
+                targetCenter.top,
+                anotherShapeCenter.centerX,
+                anotherShapeCenter.down,
+              ],
+              { stroke: "red" }
+            )
+          );
+        }
+        else if(targetCenter.down===anotherShapeCenter.top){
+          sketchRef.current._fc.add(
+            new fabric.Line(
+              [
+                targetCenter.centerX,
+                targetCenter.down,
+                anotherShapeCenter.centerX,
+                anotherShapeCenter.top,
+              ],
+              { stroke: "red" }
+            )
+          );
+        }
+        else if(targetCenter.left===anotherShapeCenter.right){
+          sketchRef.current._fc.add(
+            new fabric.Line(
+              [
+                targetCenter.left,
+                targetCenter.centerY,
+                anotherShapeCenter.right,
+                anotherShapeCenter.centerY,
+              ],
+              { stroke: "red" }
+            )
+          );
+        }
+        else if(targetCenter.right===anotherShapeCenter.left){
+          sketchRef.current._fc.add(
+          new fabric.Line(
+            [
+              targetCenter.right,
+              targetCenter.centerY,
+              anotherShapeCenter.left,
+              anotherShapeCenter.centerY,
+            ],
+            { stroke: "red" }
+          )
+        );
         }
       }
     });
