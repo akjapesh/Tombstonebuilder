0910b89f8d0ee409863f237676e7f6ba3b87fb0b
Soham Patel <soham.patel@sprinklr.com>
2022-06-28 15:54:53 +0530

    fixed rendering and updation of width,height and radius in canvas-config.

--- tombbuilder/src/components/appCanvas/canvas/hooks/useCanvasActions/useActiveItem.js
+++ tombbuilder/src/components/appCanvas/canvas/hooks/useCanvasActions/useActiveItem.js
@@ -16,23 +16,26 @@ export const useActiveItem = () => {
         case "SetCoords":
           const { target } = payLoad;
 
-          let { type, width, height, left, top, radius, rx, ry } = target;
+          let { type, width, height, left, top, radius, rx, ry, scaleX, scaleY } = target;
 
-          width = shiftValueByOffset(width);
-          radius = shiftValueByOffset(radius);
+          const width_only = shiftValueByOffset(width);
+          const height_only = shiftValueByOffset(height);
+          const radius_only = shiftValueByOffset(radius);
+          width = shiftValueByOffset(width*scaleX);
+          radius = shiftValueByOffset(radius*scaleX);
           left = shiftValueByOffset(left);
           top = shiftValueByOffset(top);
-          height = shiftValueByOffset(height);
+          height = shiftValueByOffset(height*scaleY);
 
           target.set("left", left);
           target.set("top", top);
-
+        
           if (type === "circle") {
-            target.set("radius", radius);
+            target.set("radius", radius_only);
             return setActiveItemCoords({ radius, left, top, type });
           }
-          target.set("width", width);
-          target.set("height", height);
+          target.set("width", width_only);
+          target.set("height", height_only);
           return setActiveItemCoords({
             width,
             height,
