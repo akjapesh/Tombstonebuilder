a95f43968f0028e1767624ebfd5dc5888709f421
Sanyam Srivastava <105932014+Sanyamsri@users.noreply.github.com>
2022-07-07 18:56:12 +0530

    cleaned up

--- tombbuilder/src/utils/handleShareCode.js
+++ tombbuilder/src/utils/handleShareCode.js
@@ -17,7 +17,6 @@ export const handleShareCode = async (
       },
       body: JSON.stringify({
         long_url: `${window.location.href}`,
-        // long_url: `https://tombstonebuilder.netlify.app/?data=W3sidHlwZSI6InJlY3QiLCJ2ZXJzaW9uIjoiNS4yLjEiLCJvcmlnaW5YIjoibGVmdCIsIm9yaWdpblkiOiJ0b3AiLCJsZWZ0IjoxMTIsInRvcCI6MjQwLCJ3aWR0aCI6MzIwLCJoZWlnaHQiOjIwOCwiZmlsbCI6InRyYW5zcGFyZW50Iiwic3Ryb2tlIjoiYmxhY2siLCJzdHJva2VXaWR0aCI6MCwic3Ryb2tlRGFzaEFycmF5IjpudWxsLCJzdHJva2VMaW5lQ2FwIjoiYnV0dCIsInN0cm9rZURhc2hPZmZzZXQiOjAsInN0cm9rZUxpbmVKb2luIjoibWl0ZXIiLCJzdHJva2VVbmlmb3JtIjpmYWxzZSwic3Ryb2tlTWl0ZXJMaW1pdCI6NCwic2NhbGVYIjoxLCJzY2FsZVkiOjEsImFuZ2xlIjowLCJmbGlwWCI6ZmFsc2UsImZsaXBZIjpmYWxzZSwib3BhY2l0eSI6MSwic2hhZG93IjpudWxsLCJ2aXNpYmxlIjp0cnVlLCJiYWNrZ3JvdW5kQ29sb3IiOiIiLCJmaWxsUnVsZSI6Im5vbnplcm8iLCJwYWludEZpcnN0IjoiZmlsbCIsImdsb2JhbENvbXBvc2l0ZU9wZXJhdGlvbiI6InNvdXJjZS1vdmVyIiwic2tld1giOjAsInNrZXdZIjowLCJyeCI6MCwicnkiOjB9XQ==&canvas=eyJ3aWR0aCI6NjAwLCJoZWlnaHQiOjUwMCwiYmFja2dyb3VuZENvbG9yIjoiI2YzZjNmMyIsImZvcmVncm91bmRDb2xvciI6IiNlY2ViZWIiLCJncmlkVmlzaWJpbGl0eSI6dHJ1ZSwic3BlZWQiOjJ9`,
         domain: "bit.ly",
         group_guid: "Bm778C8qhGh",
       }),
